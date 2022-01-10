c1a8a53d2bd1f7e985d6e2dc69b1f8e22668b3fd
thanhnguyen <thanhnguyentuan000@gmail.com>
2022-01-10 08:13:56 +0700

    .\btvn_lesson_9.js

---
+++ btvn_lesson_9.js
@@ -0,0 +1,56 @@
+function checkAge(number) {
+    if (number < 18) {
+        console.log(" cho hẹn hò");
+    }
+    else if (number => 18) {
+        console.log("ở nhà ngủ");
+    }
+}
+
+function TimSoNhoNhat: (number) {
+    let BaSoDaCho = a, b, c, d;
+    a = prompt("nhap so thu 1: ");
+    b = prompt("nhap so thu 2: ");
+    c = prompt("nhap so thu 3: ");
+    a = d
+    if (a <= b && a <= c) {
+        d = a;
+    } else {
+        if (b <= c)
+            d = b;
+        else
+            d = c;
+    }
+
+    // read
+    const shop_quan_ao = [
+        "ao_len",
+        "quan_dai",
+        "ao_khoac",
+      for (let a = 0; a < shop_quan_ao.length - 1; a++) {
+        console.log(shop_quan_ao[a]);
+    }
+    //  create
+    shop_quan_ao.push(prompt("nhap ten quan ao");)
+    for (let a = 0; a < shop_quan_ao.length - 1; a++) {
+        console.log(shop_quan_ao[a]);
+    }
+    //  update
+    let quan_ao_thay_the = Number(prompt("nhap vao vi tri muon sua"))
+    let ten_quan_ao = prompt("nhap vao ten thay the")
+    shop_quan_ao[quan_ao_thay_the] = ten_quan_ao.toString()
+    // delete
+    let quan_ao_het_dat = shop_quan_ao.splice(prompt("xoa ten quan ao");)
+    for (let a = 0; a < shop_quan_ao.length - 1; a++) {
+        console.log(shop_quan_ao[a]);}
+    // no
+    close()
+                
+
+
+
+
+      
+
+
+ ]
